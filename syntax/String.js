cf05560fe210aa76de2d3d073a00bdfa6df96c2e
KaseiHaku <34833442+KaseiHaku@users.noreply.github.com>
2024-12-12 03:58:31 +0800

    Update String.js

--- syntax/String.js
+++ syntax/String.js
@@ -2,4 +2,5 @@
 var literal_str = '\n';
 var literal_str2 = "\n";
 var literal_str3 = `foo${a}bar`;  // ES6 中所有字符串都是用 ` 反引号
+'\\' === '\u005C';        // true  '\u005C' 是 \ 的 unicode 码，该代码可以用来判断字符串中实际有多少个 \
 
