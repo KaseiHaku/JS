beade13b21c6a439aac5488478677a5b91b09d32
KaseiHaku <34833442+KaseiHaku@users.noreply.github.com>
2024-12-25 17:56:28 +0800

    Update StringUtil.js

--- utility/StringUtil.js
+++ utility/StringUtil.js
@@ -46,7 +46,7 @@ function StringUtil(){
     /** todo 生成一个 UUID */
     function generateUUID(){
         return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-            var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+            const r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
             return v.toString(16);
         });
     }
