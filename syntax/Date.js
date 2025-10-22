19cff561e6373a2130ae36fac582a7174f5fce66
KaseiHaku <34833442+KaseiHaku@users.noreply.github.com>
2025-10-22 15:51:38 +0800

    Update Date.js

--- syntax/Date.js
+++ syntax/Date.js
@@ -1,2 +1,7 @@
 // function Date(){}
 new Date("2012/12/25 20:11:11");  // 只能是这种格式的时间才能转换成 Date 对象
+
+
+// 获取时区
+Intl.DateTimeFormat().resolvedOptions().timeZone;
+new Intl.DateTimeFormat().resolvedOptions().timeZone;
